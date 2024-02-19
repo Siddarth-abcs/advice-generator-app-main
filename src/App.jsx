219fcbc590be8ef38ffb082184f0bb8b18096d8d
Developer Siddarth <142804820+Siddarth-abcs@users.noreply.github.com>
2024-02-19 22:45:05 +0530

    fast react app

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import hrimg from '../images/pattern-divider-desktop.svg';
+import hrimgmobile from '../images/pattern-divider-mobile.svg';
+import dice from '../images/icon-dice.svg';
+import './App.css';
+
+function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth); 
+  const [advice, setAdvice] = useState(null);
+
+  const fetchAdvice = () => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch('https://api.adviceslip.com/advice', requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        setAdvice(result);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+
+  return (
+    <>
+      <main>
+        <div className='container'>
+          <h4 className='api-head'>Advice # {advice && advice.slip && advice.slip.id}</h4>
+          <h1 className='api-text'>{advice && advice.slip && advice.slip.advice}</h1>
+          <img src={windowWidth > 768 ? hrimg : hrimgmobile} alt="" className='hr-img' />
+          <div onClick={() => fetchAdvice()} className='api-btn' style={{backgroundImage:`url(${dice})`}}>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
